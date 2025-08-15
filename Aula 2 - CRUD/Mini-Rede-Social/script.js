86fbd5a3f8000a3022222339f655ee819857e0f3
jopedrofigueiredo <joaopmfigueiredo0209@gmail.com>
2025-08-14 21:07:03 -0300

    aula2

--- Aula 2 - CRUD/Mini-Rede-Social/script.js
+++ Aula 2 - CRUD/Mini-Rede-Social/script.js
@@ -18,3 +18,82 @@ let posts = [
         date: "12/10/2023 12:00:00"
     }
 ];
+
+window.onload = function(){
+    mostrarPosts();
+    document.querySelector("#postForm").addEventListener("submit",addPost)
+}
+
+// CREATE
+function addPost(infosDoEvento){ 
+    infosDoEvento.preventDefault();
+
+    const textoPost = document.querySelector("#postText").value;
+    const categoriaPost = document.querySelector("#postCategory").value;
+    const imagemPost = document.querySelector("#postImage").value;
+    const dataPost = new Date().toLocaleString();
+
+    const novoPost = {
+        text: textoPost,
+        category: categoriaPost,
+        image: imagemPost,
+        date: dataPost
+    }
+
+
+    posts.unshift(novoPost);
+
+    mostrarPosts();
+
+}
+// READ
+function mostrarPosts(){
+    const listaPosts = document.querySelector("#postList");
+    listaPosts.innerHTML = ""
+
+    //passa em cada item do array criando um tweet
+    posts.forEach(pegarItem => {
+        const cardPost = document.createElement("div");
+        cardPost.classList.add("card");
+
+        cardPost.innerHTML = `
+            <h2>${pegarItem.text}</h2>
+            <img src="${pegarItem.image}"/>
+            <p>${pegarItem.category}</p>
+            <button>Editar</button>
+            <button>Apagar</button>
+        `
+
+        //adiciona o tweet ao HTML
+        listaPosts.append(cardPost)
+
+
+    })
+}
+
+// UPDATE
+function editarPost(){}
+// DELETE
+function deletarPost(){}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const pessoa = {
+//     nome: "joao",
+//     idade: 19,
+//     "Cidade Natal": "São Paulo",
+//     isAdmin: true
+// }
+
+// console.log(pessoa.nome);
+// console.log(pessoa["Cidade Natal"]);
